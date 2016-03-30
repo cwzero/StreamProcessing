248f85cf4e5bb758d00cc34adb8a60af7dfd5964
Eric Gjerde <gjerdeeric@gmail.com>
2016-03-30 18:39:43 -0500

    Made it a bunch better

--- src/Scanner.java
+++ src/Scanner.java
@@ -3,10 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Scanner {
 	public static Object x, y;
+
+	public static List<String> modifiers = Arrays.asList("public", "static", "abstract", "final", "volatile",
+			"protected", "private");
+	public static List<String> types = Arrays.asList("class", "enum", "interface");
 
 	public static void main(String argv[]) throws IOException {
 		InputStreamReader reader;
@@ -16,7 +21,6 @@ public class Scanner {
 			reader = new InputStreamReader(System.in);
 
 		// create the tokenizer:
-
 		StreamTokenizer tokens = new StreamTokenizer(reader);
 		tokens.eolIsSignificant(false);
 		tokens.slashStarComments(true);
@@ -25,73 +29,76 @@ public class Scanner {
 		// keep current token in variable "next":
 		int next;
 
-		String partition = "";
-		List<String> partitions = new ArrayList<String>();
+		boolean isPublic = false;
+		boolean isClass = false;
+		boolean isRetType = true;
+		boolean isField = false;
+		boolean isMethod = false;
 
 		// while more input, split input stream up into tokens and display them
 		// on stdout:
 		while ((next = tokens.nextToken()) != StreamTokenizer.TT_EOF) {
-
 			if (next == StreamTokenizer.TT_WORD) {
-				partition += tokens.sval;
-			} else if (next == StreamTokenizer.TT_NUMBER) {
-				partition += tokens.nval;
-			} else {
+				if (tokens.sval.equals("public")) {
+					isPublic = true;
+				} else if (isPublic) {
+					if (!(modifiers.contains(tokens.sval))) {
+						if (types.contains(tokens.sval)) {
+							isClass = true;
+							isPublic = false;
+						} else if (!isRetType) {
+							String name = tokens.sval;
+							List<String> addFields = new ArrayList<String>();
 
-				if (((char) next) == ';' || ((char) next) == '{' || ((char) next) == '}') {
-					process(partition);
-					partitions.add(partition);
-					partition = "";
-				} else if (((char) next) == '\'') {
-					partition += '\'' + tokens.sval + '\'';
-				} else if (((char) next) == '"') {
-					partition += '"' + tokens.sval + '"';
-				} else {
-					partition += (char) next;
-				}
-			}
+							while ((next = tokens.nextToken()) != ';' && next != '=' && next != '(') {
+								if (next == StreamTokenizer.TT_WORD) {
+									addFields.add(tokens.sval);
+								}
+							}
 
-			partition += ' ';
-		}
-	}
+							if (next == '(') {
+								isMethod = true;
+							} else {
+								isField = true;
+							}
 
-	public static void process(String partition) {
-		String classname = "";
-		if (partition.contains("public class ") && !partition.contains("\"public class \"")) {
-			int end = 0;
+							if (isMethod) {
+								while ((next = tokens.nextToken()) != ')') {
+								}
+								while ((next = tokens.nextToken()) != '{') {
+								}
+								int skip = 0;
+								while ((next = tokens.nextToken()) != '}' || skip > 0) {
+									if (next == '{')
+										skip++;
+									else if (next == '}')
+										skip--;
+								}
+								System.out.println("METHOD: " + name);
+							} else if (isField) {
+								System.out.println("FIELD: " + name);
+								for (String f : addFields) {
+									System.out.println("FIELD: " + f);
+								}
+							}
 
-			if (partition.contains("extends")) {
-				end = partition.indexOf("extends");
-			}
-			if (partition.contains("implements")) {
-				end = Math.min(end, partition.indexOf("implements"));
-			}
-
-			if (end == 0) {
-				end = partition.length();
-			}
-
-			classname = partition.substring(partition.indexOf("public class ") + "public class ".length(), end).trim();
-			System.out.println("Class: " + classname);
-		} else if (partition.contains("public ") && !partition.contains("\"public ")) {
-			if (partition.contains(" (")) {
-				String obj = partition.substring(partition.substring(0, partition.indexOf(" (")).lastIndexOf(" ") + 1, partition.indexOf(" ("));
-				if (!obj.trim().equals(classname))
-					System.out.println(obj);
-			} else {
-				if (partition.contains("=")) {
-					partition = partition.substring(0, partition.indexOf('=')).trim();
-				}
-				
-				if (partition.contains(",")) {
-					String[] obj = partition.split(",");
-					if (obj[0].contains(" ")) {
-						obj[0] = obj[0].trim().substring(obj[0].trim().lastIndexOf(' '));
+							isField = false;
+							isMethod = false;
+							isRetType = true;
+							isPublic = false;
+						} else {
+							if ((next = tokens.nextToken()) == '<') {
+								while ((next = tokens.nextToken()) != '>') {
+								}
+							} else {
+								tokens.pushBack();
+							}
+							isRetType = false;
+						}
 					}
-					
-					for (String ob: obj) {
-						System.out.println(ob.trim());
-					}
+				} else if (isClass) {
+					System.out.println("CLASS: " + tokens.sval);
+					isClass = false;
 				}
 			}
 		}
